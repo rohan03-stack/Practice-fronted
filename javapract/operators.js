a7816e0f8cbc2cdbd9d0f726a012e5da37663437
rohan03-stack <ronygaikwad03@gmail.com>
2025-08-17 21:36:23 +0530

    data is added

--- javapract/operators.js
+++ javapract/operators.js
@@ -36,12 +36,89 @@
 
 //   ......conditional rendering...
 
-let a = 25
+// let a = 25
 
-if(a >18) {
-    console.log("you can vote");
+// if(a >18) {
+//     console.log("you can vote");
     
-}
+// }
+
+// let mode = "dark";
+// let color;
+
+// if(mode === "dark"){
+//     color = "black"
+// }
+
+// if (mode=== "white"){
+//     color = "white"
+// }
+
+// console.log(color);
+
+
+
+
+// .................if else statement .................
+
+
+// let mode = "light";
+// let color;
+
+// if(mode === "dark"){
+//     color = black;
+// } else{
+//     color = "white";
+// }
+
+// console.log(color
+
+// );
+
+
+// let age = 25;
+
+// if (age >=18){
+//     console.log("vote");
+    
+// }else{
+//     console.log("not vote");
+    
+// }
+// .........else if statement ......................
+
+
+//    let mode = "dk";
+//    let  color;
+
+//    if (mode === "dark"){
+//     color="black";
+//    }else if(mode === "blue"){
+//     color= "blue";
+//    }else if(mode === "red"){
+//     color = "red";
+//    }else{
+//     color = "white";
+//    }
+
+//    console.log(color);
+   
+
+
+//    ............terneray operators.............
+
+
+let age = 15;
+
+let result = age > 18 ? "adult" : "not adilt";
+console.log(result);
+
+
+
+
+
+// ................practice.............
+
 
 
 
