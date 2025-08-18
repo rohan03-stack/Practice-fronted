06a33132e261a6c3a60ff7849c3999017e00dff3
rohan03-stack <ronygaikwad03@gmail.com>
2025-08-18 21:12:58 +0530

    java file is added

---
+++ javapract/loops.js
@@ -0,0 +1,24 @@
+// for loop
+
+// for (let count =1 ; count <=10; count++){
+//  console.log("apna colledge");
+ 
+// }
+
+
+// calculate sum 1 t0 5;
+ 
+// let sum = 0;
+//  for(let i = 5; i<=10; i++){
+//     sum = sum + i
+//  }
+//  console.log(sum);
+ 
+
+// for (let i = 1; i<=5; i++){
+//     console.log(i);
+    
+// }
+
+
+// ..............infinite loop...........
