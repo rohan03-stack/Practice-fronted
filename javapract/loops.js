1df52f1822086afb90ed0db9853414813445180d
rohan03-stack <ronygaikwad03@gmail.com>
2025-08-19 22:14:19 +0530

    data is added

--- javapract/loops.js
+++ javapract/loops.js
@@ -21,4 +21,83 @@
 // }
 
 
-// ..............infinite loop...........
+// ..............while loop...........
+
+// let i = 1;
+
+// while(i<=5){
+//     console.log(i);
+//      i++;
+// }
+
+
+// ................do while loop..........  
+// let i = 1;
+
+// do{
+//     console.log(i);
+//     i++
+// }while(i<=10);
+
+
+
+// .....................for-of loop...........
+
+
+// let str = "rohan";
+
+// for(let i of str){
+//     console.log("i=",i);
+    
+// }
+
+
+// let str = "rohan";
+// let size = 0;
+
+// for(let i of str){
+//     console.log(i);
+//     size++;
+// }
+
+// console.log(size);
+
+
+// for in loop
+
+// let student = {
+//     name: "rohan gaikwad",
+//     age: 20,
+//     cgpa: 9.1,
+//     isPass: true
+// };
+
+// for(let key in student){
+//     console.log(key, student[key]);
+    
+// }
+
+//   ................find prime Number.................
+// for (let num=0; num<=100; num++){
+//   if(num%2===0){
+//     console.log(num);
+    
+//   }else{
+   
+
+    
+//   }
+    
+// }
+
+
+// practice q2
+
+let gamenum = 20;
+let useNum = prompt("guess the number")
+
+while(useNum != 
+    gamenum){
+    useNum =prompt("you enter wrong number")
+}
+console.log("congratulation");
---
+++ javapract/string.js
@@ -0,0 +1,5 @@
+// string lenght
+
+
+let str = "gaikwad";
+console.log(str[1]);
